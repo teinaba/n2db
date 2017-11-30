6129e34b1add378648788f409df718a60825c0b7
tetsu <inaba.t@a.phys.nagoya-u.ac.jp>
2017-11-30 13:53:41 +0900

    Initial commit

---
+++ n2gdrive.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+# import modules
+# --------------
+import pydrive
+
+
+class n2gdrive(object):
+
+    def __init__(self):
+        pass
+
+    def
---
+++ n2gspread.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+# import modules
+# --------------
+import gspread
+
+
+class n2gspread(object):
+
+    def __init__(self):
+        pass
+
+    def
